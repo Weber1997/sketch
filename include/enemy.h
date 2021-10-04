cbed20fd395a4276b3bb7646ed5419e91eb51c9d
Weber1997 <w15768757688@outlook.com>
2021-10-04 22:18:40 +0800

    add class enemy

---
+++ include/enemy.h
@@ -0,0 +1,21 @@
+#ifndef _ENEMY_H
+#define _ENEMY_H
+
+class enemy
+{
+private:
+    /* data */
+public:
+    enemy(/* args */);
+    ~enemy();
+};
+
+enemy::enemy(/* args */)
+{
+}
+
+enemy::~enemy()
+{
+}
+
+#endif
